3e71be6477e361577d6d886e5146025518b9243c
peric <peric.drazhen@gmail.com>
2018-11-15 18:23:48 +0100

    Better seeds

--- db/less_seeds/budgets.rb
+++ db/less_seeds/budgets.rb
@@ -16,11 +16,11 @@ end
 def add_image_to(imageable)
   # imageable should respond to #title & #author
   imageable.image = Image.create!({
-    imageable: imageable,
-    title: imageable.title,
-    attachment: INVESTMENT_IMAGE_FILES.sample,
-    user: imageable.author
-  })
+                                    imageable: imageable,
+                                    title: imageable.title,
+                                    attachment: INVESTMENT_IMAGE_FILES.sample,
+                                    user: imageable.author
+                                  })
   imageable.save
 end
 
@@ -61,9 +61,13 @@ section "Creating Budgets" do
 
   Budget.all.each do |budget|
     budget.phases.all.each do |phase|
+      enabled = true
+      summary = "#{phase.kind.capitalize} summary"
+      description = "#{phase.kind.capitalize} description"
       if PHASES_TO_DISABLE.include?(phase.kind)
-        phase.update(enabled: false)
+        enabled = false
       end
+      phase.update(enabled: enabled, summary: summary, description: description)
     end
   end
 end
@@ -71,7 +75,7 @@ end
 section "Creating Investments" do
   tags = Faker::Lorem.words(10)
   100.times do
-    heading = Budget::Heading.all.sample
+    heading = Budget.last.headings.all.sample
 
     investment = Budget::Investment.create!(
       author: User.all.sample,
@@ -103,8 +107,8 @@ end
 section "Geolocating Investments" do
   Budget.find_each do |budget|
     budget.investments.each do |investment|
-      MapLocation.create(latitude: Setting['map_latitude'].to_f + rand(-10..10)/100.to_f,
-                         longitude: Setting['map_longitude'].to_f + rand(-10..10)/100.to_f,
+      MapLocation.create(latitude: Setting['map_latitude'].to_f + rand(-10..10) / 100.to_f,
+                         longitude: Setting['map_longitude'].to_f + rand(-10..10) / 100.to_f,
                          zoom: Setting['map_zoom'],
                          investment_id: investment.id)
     end
