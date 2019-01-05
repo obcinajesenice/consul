b5f5b5d4433f30d592c8e9f8a9804250908fd0e4
peric <peric.drazhen@gmail.com>
2019-01-05 20:10:15 +0100

    Introduced phases tabs

--- app/controllers/custom/welcome_controller.rb
+++ app/controllers/custom/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   include CustomPhasesHelper
   skip_authorization_check
 
+  before_action :load_custom_phases, only: [:index]
+
   layout "devise", only: [:verification]
 
   def index
--- app/helpers/custom_phases_helper.rb
+++ app/helpers/custom_phases_helper.rb
@@ -5,8 +5,12 @@ module CustomPhasesHelper
   CUSTOM_PHASE_BALLOTING = :balloting
   CUSTOM_PHASE_FINISHED = :finished
 
-  CustomPhase = Struct.new(:kind, :summary, :description, :url, :enabled) do
+  CustomPhase = Struct.new(:kind, :summary, :description, :starts_at, :ends_at, :url, :enabled) do
 
+  end
+
+  def load_custom_phases
+    @custom_phases = custom_phases
   end
 
   def custom_phases
@@ -27,7 +31,7 @@ module CustomPhasesHelper
       CUSTOM_PHASE_BALLOTING => true,
       CUSTOM_PHASE_FINISHED => true,
     }.each do |phase, enabled|
-      custom_phases[phase] = CustomPhase.new(phase, nil, nil, nil, enabled)
+      custom_phases[phase] = CustomPhase.new(phase, nil, nil, nil, nil, nil, enabled)
     end
     custom_phases
   end
@@ -35,12 +39,20 @@ module CustomPhasesHelper
   def custom_phases_descriptions(custom_phases)
     custom_phases[CUSTOM_PHASE_ACCEPTING].summary = current_budget.phases.accepting.summary
     custom_phases[CUSTOM_PHASE_ACCEPTING].description = current_budget.phases.accepting.description
+    custom_phases[CUSTOM_PHASE_ACCEPTING].starts_at = current_budget.phases.accepting.starts_at
+    custom_phases[CUSTOM_PHASE_ACCEPTING].ends_at = current_budget.phases.accepting.ends_at
     custom_phases[CUSTOM_PHASE_SELECTING].summary = current_budget.phases.selecting.summary
     custom_phases[CUSTOM_PHASE_SELECTING].description = current_budget.phases.selecting.description
+    custom_phases[CUSTOM_PHASE_SELECTING].starts_at = current_budget.phases.selecting.starts_at
+    custom_phases[CUSTOM_PHASE_SELECTING].ends_at = current_budget.phases.selecting.ends_at
     custom_phases[CUSTOM_PHASE_BALLOTING].summary = current_budget.phases.balloting.summary
     custom_phases[CUSTOM_PHASE_BALLOTING].description = current_budget.phases.balloting.description
+    custom_phases[CUSTOM_PHASE_BALLOTING].starts_at = current_budget.phases.balloting.starts_at
+    custom_phases[CUSTOM_PHASE_BALLOTING].ends_at = current_budget.phases.balloting.ends_at
     custom_phases[CUSTOM_PHASE_FINISHED].summary = current_budget.phases.finished.summary
     custom_phases[CUSTOM_PHASE_FINISHED].description = current_budget.phases.finished.description
+    custom_phases[CUSTOM_PHASE_FINISHED].starts_at = current_budget.phases.finished.starts_at
+    custom_phases[CUSTOM_PHASE_FINISHED].ends_at = current_budget.phases.finished.ends_at
     custom_phases
   end
 
