baf2d193b7b5e91f7e6637f9f8d04732e0933dea
klemensek <klemensek@gmail.com>
2019-01-11 12:42:11 -0300

    footer, custom-phases-nav

--- app/assets/javascripts/custom/equalizer.js
+++ app/assets/javascripts/custom/equalizer.js
@@ -1,12 +1,12 @@
-$(function(){
+$(document).on('page:change', function () {
   var c = $("#custom_phases_tabs li");
-  if(c.length == 3){
+  if (c.length == 3) {
     $(".custom-phases-nav .tabs li").css({
       width: "32.8%"
     })
   }
 
-  if($(".budget-investments .budget-investment-holder").length > 0) {
+  if ($(".budget-investments .budget-investment-holder").length > 0) {
     var mh = 0;
     var bdesc = $(".budget-investments .budget-investment-holder .budget-investment-content");
     bdesc.each(function () {
@@ -14,12 +14,11 @@ $(function(){
         mh = $(this).height();
       }
     });
-    if(mh > 0) {
+    if (mh > 0) {
       bdesc.height(mh);
     }
   }
-  if($(".welcome-column .linkable").length > 0){
+  if ($(".welcome-column .linkable").length > 0) {
     $(".welcome-column .linkable").height($(".welcome-column").height());
   }
-
 });
