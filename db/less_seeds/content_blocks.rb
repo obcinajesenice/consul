52e266c27f148e85dff96532fdbfd6595a2909f0
Dražen Perić <drazen.peric@kiwi.com>
2019-01-19 17:43:25 +0100

    Translations and translations-related stuff

--- db/less_seeds/content_blocks.rb
+++ db/less_seeds/content_blocks.rb
@@ -1,3 +1,3 @@
 section "Content blocks" do
-  SiteCustomization::ContentBlock.create(name: 'top_links', locale: 'sl-SI', body: '<li><a href="http://example.com">Občina Neobstoj</a></li>')
+  # SiteCustomization::ContentBlock.create(name: 'top_links', locale: 'sl-SI', body: '<li><a href="http://example.com">Občina Neobstoj</a></li>')
 end
