fa103fe7df16ce9ab131d77e0aa29b665f28dde2
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-01-24 10:40:29 +0100

    check for age from emšo

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -1,3 +1,4 @@
+require 'date'
 require_dependency Rails.root.join('app', 'models', 'user').to_s
 
 class User < ActiveRecord::Base
@@ -10,7 +11,23 @@ class User < ActiveRecord::Base
 
   private
 
-  def valid_emso_number?
+  def date_from_emso(emso)
+    if emso[4] == 0
+      return Date.parse('2' + emso[4] + emso[5] + emso[6] + '-' + emso[2] + emso[3] + '-' + emso[0] + emso[1])
+    else
+      return Date.parse('1' + emso[4] + emso[5] + emso[6] + '-' + emso[2] + emso[3] + '-' + emso[0] + emso[1])
+    end
+  end
+  
+  def is_old_enough(birthday)
+    date_limit = Date.parse('2005-02-26')
+    if (date_limit < birthday)
+      return false
+    end
+    return true
+  end
+  
+  def valid_emso_number
     if /[0,1,2,3]\d[0,1]\d[0,9]\d\d\d{6}/.match(document_number)
       if document_number[12].to_i == 11 - (
         (
@@ -22,7 +39,7 @@ class User < ActiveRecord::Base
           ((document_number[5].to_i + document_number[11].to_i) * 2)
         ) % 11
       )
-        return true
+        return is_old_enough(date_from_emso(document_number))
       elsif document_number[12].to_i == (
           ((document_number[0].to_i + document_number[6].to_i) * 7) + 
           ((document_number[1].to_i + document_number[7].to_i) * 6) + 
@@ -31,7 +48,7 @@ class User < ActiveRecord::Base
           ((document_number[4].to_i + document_number[10].to_i) * 3) + 
           ((document_number[5].to_i + document_number[11].to_i) * 2)
         ) % 11
-          return true
+          return is_old_enough(date_from_emso(document_number))
       else
         return false
       end
