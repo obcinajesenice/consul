0e05ccaa708d9a12567f26186d44f41f907cfb5d
Dražen Perić <drazen.peric@kiwi.com>
2019-02-04 21:05:33 +0100

    residence.rb fix

--- app/models/custom/officing/residence.rb
+++ app/models/custom/officing/residence.rb
@@ -4,9 +4,11 @@ class Officing::Residence
 
   # remove existing validators
   self.class_eval do
-    _validators[:year_of_birth]
-      .attributes
-      .delete(:year_of_birth)
+    puts _validators[:year_of_birth]
+    # _validators[:year_of_birth]
+    #   .find { |v| v.is_a? ActiveRecord::Validations::PresenceValidator }
+    #   .attributes
+    #   .delete(:year_of_birth)
   end
 
   def retrieve_census_data
