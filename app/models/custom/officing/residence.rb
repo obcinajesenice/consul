de436d639e206a0bd81af81d0f979075d287ba4b
Dražen Perić <drazen.peric@kiwi.com>
2019-02-04 20:54:22 +0100

    residence.rb update

--- app/models/custom/officing/residence.rb
+++ app/models/custom/officing/residence.rb
@@ -2,14 +2,17 @@ require_dependency Rails.root.join('app', 'models', 'officing', 'residence').to_
 
 class Officing::Residence
 
-  reset_callbacks(:before_validation)
-
   # remove existing validators
   self.class_eval do
     _validators[:year_of_birth]
       .find { |v| v.is_a? ActiveRecord::Validations::PresenceValidator }
       .attributes
       .delete(:year_of_birth)
+  end
+
+  def retrieve_census_data
+    nil
+    #@census_api_response = CensusCaller.new.call(document_type, document_number)
   end
 
   def allowed_age
