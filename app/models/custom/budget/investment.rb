48b6f27f37606c68e5afc1e3d7477027210e92fb
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-02-22 10:33:21 +0100

    trying to raise max number of documents

--- app/models/custom/budget/investment.rb
+++ app/models/custom/budget/investment.rb
@@ -3,6 +3,11 @@ require_dependency Rails.root.join('app', 'models', 'budget', 'investment').to_s
 class Budget
   class Investment < ActiveRecord::Base
 
+    # change some defaults
+    documentable max_documents_allowed: 5,
+                 max_file_size: 3.megabytes,
+                 accepted_content_types: [ "application/pdf" ]
+
     # remove existing validator
     self.class_eval do
       _validators[:title]
