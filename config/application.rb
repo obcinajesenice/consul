a8097cf9fda7b2a2f476474a9c24db8b18b5b07f
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-08-27 15:27:18 +0200

    turn on italian locale

--- config/application.rb
+++ config/application.rb
@@ -19,32 +19,17 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :sl
     available_locales = [
-      "ar",
-      "de",
-      "en",
-      "es",
-      "fa",
-      "fr",
-      "gl",
-      "he",
-      "it",
-      "nl",
-      "pl",
-      "pt-BR",
-      "sq",
-      "sv",
-      "val",
-      "zh-CN",
-      "zh-TW"]
+      "sl",
+      "it"]
     config.i18n.available_locales = available_locales
-    config.i18n.fallbacks = {
-      'fr'    => 'es',
-      'gl'    => 'es',
-      'it'    => 'es',
-      'pt-BR' => 'es'
-    }
+    # config.i18n.fallbacks = {
+    #   'fr'    => 'es',
+    #   'gl'    => 'es',
+    #   'it'    => 'es',
+    #   'pt-BR' => 'es'
+    # }
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'custom', '**', '*.{rb,yml}')]
 
