312be608c104ccdbdcbf4aa7f90bf41270216d7e
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-09-01 16:52:10 +0200

    new terms and anonymity handling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190816130803) do
+ActiveRecord::Schema.define(version: 20190901141028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1404,6 +1404,7 @@ ActiveRecord::Schema.define(version: 20190816130803) do
     t.boolean  "recommended_proposals",                     default: true
     t.text     "phone"
     t.text     "address"
+    t.boolean  "is_anonymous",                              default: false,                 null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
