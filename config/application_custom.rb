48213746a909eae9a521b5084acff2c09fac1e9f
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-09-02 12:52:44 +0200

    remove english locale from available locales

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,7 +2,7 @@ module Consul
   class Application < Rails::Application
     config.i18n.default_locale = 'sl'
     # available_locales = %w(ar de en es fa fr gl he it nl pl pt-BR sq sv val zh-CN zh-TW sl sl-SI)
-    available_locales = %w(en sl it)
+    available_locales = %w(sl it)
     config.i18n.available_locales = available_locales
     config.i18n.fallbacks = {
       # 'fr' => 'es',
