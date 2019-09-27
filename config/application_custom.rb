d5ae137ecb314940676cec57a14cc693aabb6a12
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-09-27 07:43:26 +0200

    limit available locales

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = 'sl'
+    available_locales = %w(sl it en)
+    config.i18n.available_locales = available_locales
   end
 end
