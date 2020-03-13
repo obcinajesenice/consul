9d456587557468824b86614fba080965ec7c46e0
obcinajesenice <obcina.jesenice@jesenice.si>
2020-03-13 11:14:27 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 # Default admin user (change password after first deploy to a server!)
 if Administrator.count == 0 && !Rails.env.test?
-  admin = User.create!(username: 'admin', email: 'admin@consul.dev', password: '12345678', password_confirmation: '12345678', confirmed_at: Time.current, terms_of_service: "1")
+  admin = User.create!(username: 'admin', email: 'admin@consul.dev', password: '12345678', password_confirmation: '12345678', confirmed_at: Time.current, document_number:'1206983500006', data_consent: '1', terms_of_service: "1")
   admin.create_administrator
 end
 
